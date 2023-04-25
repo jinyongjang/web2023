79ebff637c940da4cc7150bb78f07b4e413bb57b
jinyongjang <ebmdols1@gmail.com>
2023-04-25 19:34:25 +0900

    2023-04-25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,53 @@
+const allMusic = [
+    {
+        name :"1. 마마마마",
+        artist : "Easda sas",
+        img : "music_view01",
+        audio : "music_audio01"
+    },{
+        name :"2. 마마마마",
+        artist : "Easda sas",
+        img : "music_view02",
+        audio : "music_audio02"
+    },{
+        name :"3. 마마마마",
+        artist : "Easda sas",
+        img : "music_view03",
+        audio : "music_audio03"
+    },{
+        name :"4. 마마마마",
+        artist : "Easda sas",
+        img : "music_view04",
+        audio : "music_audio04"
+    },{
+        name :"5. 마마마마",
+        artist : "Easda sas",
+        img : "music_view05",
+        audio : "music_audio05"
+    },{
+        name :"6. 마마마마",
+        artist : "Easda sas",
+        img : "music_view06",
+        audio : "music_audio06"
+    },{
+        name :"7. 마마마마",
+        artist : "Easda sas",
+        img : "music_view07",
+        audio : "music_audio07"
+    },{
+        name :"8. 마마마마",
+        artist : "Easda sas",
+        img : "music_view08",
+        audio : "music_audio08"
+    },{
+        name :"9. 마마마마",
+        artist : "Easda sas",
+        img : "music_view09",
+        audio : "music_audio09"
+    },{
+        name :"10. 마마마마",
+        artist : "Easda sas",
+        img : "music_view10",
+        audio : "music_audio10"
+    }
+]
