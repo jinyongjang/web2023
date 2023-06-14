26b85e889c3b2129d2d38de21998652cd6db01f9
jinyongjang <ebmdols1@gmail.com>
2023-06-14 19:11:05 +0900

    2023-06-14

---
+++ webbd/webdtest4/js/script.js
@@ -0,0 +1,19 @@
+let i = 0
+setInterval(() => {
+    if(i < 2){
+        i++;
+    } else {
+        i = 0;
+    }
+    $(".slide__inner").animate({left:-2000 * i +"px"},500)
+}, 3000)
+
+let y = 0
+setInterval(() => {
+    if(y < 2){
+        y++;
+    } else {
+        y = 0
+    }
+    $(".card2__wrap").animate({left:-450 * y + "px"},300)
+}, 3000)
